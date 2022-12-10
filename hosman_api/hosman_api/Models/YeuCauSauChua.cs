2cabf58883863ffcf0c7faf2edf56f043a7c2457
TuyeetVy <102270047+TuyeetVy@users.noreply.github.com>
2022-12-10 17:12:41 +0700

    ADD YeuCauSuaChuaVM

--- hosman_api/hosman_api/Models/YeuCauSauChua.cs
+++ hosman_api/hosman_api/Models/YeuCauSauChua.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 
 namespace hosman_api.Models;
 
-public partial class YeuCauSauChua
+public class YeuCauSuaChuaVM
 {
-    public string MaYeuCau { get; set; } = null!;
-
     public string ChiTiet { get; set; } = null!;
 
     public string HinhAnh { get; set; } = null!;
@@ -16,6 +14,10 @@ public partial class YeuCauSauChua
     public int? TinhTrang { get; set; }
 
     public string MaHopDong { get; set; } = null!;
+}
+public partial class YeuCauSauChua : YeuCauSuaChuaVM
+{
+    public string MaYeuCau { get; set; } = null!;
 
     public virtual HopDongThue MaHopDongNavigation { get; set; } = null!;
 }
